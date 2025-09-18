b48da301f309789ea0f5565fa9ea0378cdf8243b
Sjoerd de Vries <sjoerd.devries@loria.fr>
2025-09-18 11:30:21 +0200

    make fit more robust for upstream errors. Calculate missing fits

--- allpdb-rna-fit-dinuc.py
+++ allpdb-rna-fit-dinuc.py
@@ -30,7 +30,17 @@ is_nuc1 = np.array([c == motif[0] for c in seq], bool)
 is_nuc2 = np.array([c == motif[1] for c in seq], bool)
 same_seg = np.equal(nucseg[:-1], nucseg[1:])
 
-is_dinuc = np.where(is_nuc1[:-1] & is_nuc2[1:] & same_seg)[0]
+is_dinuc0 = np.where(is_nuc1[:-1] & is_nuc2[1:] & same_seg)[0]
+is_dinuc = []
+for pos in is_dinuc0:
+    try:
+        start, end = nucstart[pos : pos + 3 : 2]
+    except ValueError:
+        # print("ERR", pos)
+        continue
+    is_dinuc.append(pos)
+is_dinuc = np.array(is_dinuc)
+
 
 pos_ori = [nucsegnames[pos][:4] for pos in is_dinuc]
 
@@ -51,18 +61,21 @@ assert len(lib_ori_ext) == len(lib_ext)
 
 from nefertiti.functions.superimpose import superimpose_array
 
-best_conf = np.empty(len(is_dinuc), np.uint16)
+best_conf = np.empty(len(is_dinuc), np.int32)
 best_rmsd = np.empty(len(is_dinuc), np.float32)
 for posnr, pos in enumerate(tqdm(is_dinuc, desc="Primary library")):
     start, end = nucstart[pos : pos + 3 : 2]
-    ### assert end - start == lib.shape[1], (pos, end - start)
+    if end - start != lib.shape[1]:
+        best_conf[posnr] = -1
+        best_rmsd[posnr] = 0
+        continue
     c = coor[start:end]
     _, rmsd = superimpose_array(lib, c)
     best = rmsd.argmin()
     best_conf[posnr] = best
     best_rmsd[posnr] = rmsd[best]
 
-best_ori = [lib_ori[best] for best in best_conf]
+best_ori = [lib_ori[best] if best != -1 else None for best in best_conf]
 same_ori = np.array([ori1 == ori2 for ori1, ori2 in zip(best_ori, pos_ori)])
 
 replacement_mask = np.zeros(len(is_dinuc), bool)
@@ -94,7 +107,7 @@ lib_ori_ext = np.array(lib_ori_ext)
 for posnr_ext, posnr in enumerate(tqdm(poor_fit_posnr, desc="Extension library")):
     pos = is_dinuc[posnr]
     start, end = nucstart[pos : pos + 3 : 2]
-    ### assert end - start == lib.shape[1], (pos, end - start)
+    assert end - start == lib.shape[1], (pos, end - start)
 
     curr_ori = pos_ori[posnr]
     ori_mask = np.equal(lib_ori_ext, curr_ori)
@@ -117,6 +130,8 @@ print("Improved fit from extension library:", improv_mask.sum())
 with open(f"allpdb-rna-fit-dinuc-{motif}.txt", "w") as f:
     for n, pos in enumerate(is_dinuc):
         conf = best_conf[n]
+        if conf is None:
+            continue
         rmsd = best_rmsd[n]
         replacement = int(replacement_mask[n])
         print(f"{pos+1} {conf+1} {rmsd:.5f} {replacement}", file=f)
